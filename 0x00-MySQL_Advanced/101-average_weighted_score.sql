3d880aa61377c24ab32749d606b193a1fe6884b1
fhayvy <ezefavourchiamaka01@gmail.com>
2024-10-17 17:37:19 +0100

    task 0

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,3 +1,5 @@
+-- script that creates a table users with follwing fields
+-- id, email, name
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
